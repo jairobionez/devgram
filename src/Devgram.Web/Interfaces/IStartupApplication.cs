bb6cb786d14882ef255e178e225b313735bce861
jairobionez <jairo@treeinova.com.br>
2024-11-08 12:04:21 -0300

    rm startup mvc

--- src/Devgram.Web/Interfaces/IStartupApplication.cs
+++
@@ -1,9 +0,0 @@
-namespace Devgram.Web.Interfaces
-{
-    public interface IStartupApplication
-    {
-        IConfiguration Configuration { get; }
-        void ConfigureServices(IServiceCollection services);
-        void Configure(WebApplication app, IWebHostEnvironment env);
-    }
-}
--- src/Devgram.Web/Program.cs
+++ src/Devgram.Web/Program.cs
@@ -1,5 +1,28 @@
 
+using Devgram.Auth.Configuration;
+using Devgram.Data.Infra;
 using Devgram.Web;
+using Devgram.Web.Configuration;
 
-var builder = WebApplication.CreateBuilder(args)
-                .UseStartup<Startup>();
+// Declaração do builder
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurando o AppSettings.json conforme ambiente de execução
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDependencyConfig();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddWebConfig(builder.Configuration);
+builder.Services.AddIdentityConfig(builder.Configuration);
+builder.Services.DbInitializer();
+
+var app = builder.Build();
+
+app.UseWebConfig(app.Environment);
+
+app.Run();
